63133f1731a9da2e57cc156ff53e4c2039b85ff4
20171105409ani <2509296163@qq.com>
2018-10-27 09:37:25 +0800

    Initial Commit

---
+++ result/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  result
+//
+//  Created by s20171105409 on 2018/10/27.
+//  Copyright © 2018 s20171105409. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
